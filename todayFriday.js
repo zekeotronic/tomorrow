4007149302d673299756f4fd77320c9d6888008c
Eric Ihle <zekeotronic@gmail.com>
2020-07-29 21:13:00 -0400

    Init

---
+++ todayFriday.js
@@ -0,0 +1,28 @@
+document.getElementById('sunday').addEventListener('click', function(){
+  document.getElementById("result").innerHTML = 'Today is Sunday? Tomorrow is Monday.';
+});
+
+document.getElementById('monday').addEventListener('click', function(){
+  document.getElementById("result").innerHTML = 'Today is Monday? Tomorrow is Tuesday.';
+});
+
+document.getElementById('tuesday').addEventListener('click', function(){
+  document.getElementById("result").innerHTML = 'Today is Tuesday? Tomorrow is Wednesday.';
+});
+
+document.getElementById('wednesday').addEventListener('click', function(){
+  document.getElementById("result").innerHTML = 'Today is Wednesday? Tomorrow is Thursday.';
+});
+
+document.getElementById('thursday').addEventListener('click', function(){
+  document.getElementById("result").innerHTML = 'Today is Thursday? Tomorrow is Friday.';
+});
+
+document.getElementById('friday').addEventListener('click', function(){
+  document.getElementById("result").innerHTML = 'Today is Friday? Tomorrow is Saturday.';
+});
+
+document.getElementById('saturday').addEventListener('click', function(){
+  document.getElementById("result").innerHTML = 'Today is Saturday? Tomorrow is Sunday.';
+});
+
